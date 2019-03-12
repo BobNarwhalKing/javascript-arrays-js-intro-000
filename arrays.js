94f61d02304be7f400cf1cf4139e5e25b0a47fa0
Kurtis Leach <danceflurry4366@gmail.com>
2019-03-12 18:00:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -17,8 +17,7 @@ function accessElementInArray (array, index) {
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray (array) {
-  array.shift()
-  return array
+  return array.shift()
 }
 function removeElementFromBeginningOfArray (array) {
   return array.slice(1)
