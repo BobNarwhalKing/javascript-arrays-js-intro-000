84df5513f79fc26bc80352445fddacd2120aa75e
Kurtis Leach <danceflurry4366@gmail.com>
2019-03-12 17:28:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray () {
   return ["twix", ...chocolateBars];
 }
+function destructivelyAddElementToBeginningOfArray () {
+  chocolateBars[twix] = twix
+  return chocolateBars
+}
