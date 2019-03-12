481ed70709f0cc5d8f6491c45a69dd61f10a2922
Kurtis Leach <danceflurry4366@gmail.com>
2019-03-12 17:33:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,8 +2,3 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray (element, chocolateBars) {
   return [`${element}`, ...chocolateBars]
 }
-function destructivelyAddElementToBeginningOfArray () {
-  var myArray = [1, 2, 3]
-  myArray[5] = 5
-  return myArray
-}
